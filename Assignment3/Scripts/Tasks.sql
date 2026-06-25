c86e10cc85f2ab476f4080d02e6e5a994f0e857f
Unitmaps <lagirmaks@gmail.com>
2026-06-25 11:12:14 +0300

    Add create_order procedure

--- Assignment3/Scripts/Tasks.sql
+++ Assignment3/Scripts/Tasks.sql
@@ -9,3 +9,14 @@ from order_items
 where order_id = p_order_id
 
 $$;
+
+create or replace procedure create_order(p_customer_id int)
+language plpgsql 
+as $$
+begin
+if not exists (select 1 from customers where customer_id = p_customer_id)
+then return;
+end if;
+insert into orders (customer_id, order_date, total_amount) values(p_customer_id, now(), 0);
+end;
+$$;
