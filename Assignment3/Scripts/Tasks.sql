f8f85f466c5f6ac165b21db80a170d964afb29ca
Unitmaps <lagirmaks@gmail.com>
2026-06-25 21:39:02 +0300

    Add triggers for updating total order and logging

--- Assignment3/Scripts/Tasks.sql
+++ Assignment3/Scripts/Tasks.sql
@@ -6,7 +6,7 @@ as $$
 
 select coalesce(SUM(quantity * price), 0)
 from order_items
-where order_id = p_order_id
+where order_id = p_order_id;
 
 $$;
 
@@ -20,3 +20,55 @@ end if;
 insert into orders (customer_id, order_date, total_amount) values(p_customer_id, now(), 0);
 end;
 $$;
+
+create or replace procedure add_product_to_order(
+    p_order_id int,
+    p_product_id int,
+    p_quantity int
+)
+language plpgsql 
+as $$
+begin
+if (p_quantity <= 0) or not exists (select 1 from products where product_id = p_product_id and stock_quantity >= p_quantity)
+then return;
+end if;
+insert into order_items (order_id, product_id, quantity, price) values(p_order_id, p_product_id, p_quantity, (select price from products where product_id = p_product_id));
+update products set stock_quantity = stock_quantity - p_quantity where product_id = p_product_id;
+end;
+$$;
+
+create or replace function upd_total_order()
+returns trigger
+language plpgsql
+as $$
+begin
+update orders set total_amount = calculate_order_total(order_id) where order_id = order_id or order_id = old.order_id;
+return new;
+end;
+$$;
+
+create or replace trigger trg_ins_del_total_order
+after insert or delete on order_items
+for each row
+execute function upd_total_order();
+
+create or replace trigger trg_u_total_order
+after update on order_items
+for each row
+when (new.price is distinct from old.price or new.quantity is distinct from old.quantity)
+execute function upd_total_order();
+
+create or replace function add_to_log()
+returns trigger
+language plpgsql
+as $$
+begin
+insert into order_log (order_id, customer_id, action, log_date) values (new.order_id, new.customer_id, 'new order', now());
+return new;
+end;
+$$;
+
+create or replace trigger trg_logging
+after insert on orders
+for each row
+execute function add_to_log();
